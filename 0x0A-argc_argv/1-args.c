0139f14e94f28973e98de20c8290e1da0348f5ed
Amadou001 <bamadou634@gmail.com>
2023-11-17 22:08:10 +0800

    C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,10 @@
 * main - prints the name of the program
 * @argc: number of arguments
 * @argv: array of arguments
-*
 * Return: Always 0 (Success)
 */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 return (0);
 }
