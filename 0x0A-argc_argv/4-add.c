52280ea3a0bd268ed15164e8de0dfbf7c3657391
Amadou001 <bamadou634@gmail.com>
2023-11-17 23:35:22 +0800

    C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,17 +9,20 @@
 */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
+int i, j, sum = 0;
 if (argc - 1 == 0)
 {
 printf("0\n");
 }
 for (i = 1; i < argc; i++)
 {
-if (!isdigit(argv[i]))
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
+}
 }
 sum = sum + atoi(argv[i]);
 }
