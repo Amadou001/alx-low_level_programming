01752244b3bdf095842a6a83320e2b8cd37a9f61
Amadou001 <bamadou634@gmail.com>
2023-11-17 22:41:09 +0800

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* main - prints the name of the program
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 int num1, num2, mul;
@@ -11,7 +16,7 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-mul = num1 * num2;
+mul = num1 *num2;
 printf("%d\n", mul);
 return (0);
 }
