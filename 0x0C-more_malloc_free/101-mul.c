2e01afbbe7953922b2dd89874025e6e8b7ee590c
Amadou001 <bamadou634@gmail.com>
2023-11-24 01:01:40 +0800

    C - More malloc, free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+/**
+* main - multiply two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int mul, num1, num2, i, j;
+if (argc - 1 != 2)
+{
+printf("Error\n");
+exit(98);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+exit(98);
+}
+}
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+mul = num1 *num2;
+printf("%d\n",mul);
+return (0);
+}
