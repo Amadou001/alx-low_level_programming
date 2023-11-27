78bbc55dde65a8529feb82bdf15d9e3d7335255d
Amadou001 <bamadou634@gmail.com>
2023-11-27 22:10:19 +0800

    C -Structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,15 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d = (struct dog *)calloc(3,sizeof(struct dog));
+d = (struct dog *)calloc(3, sizeof(struct dog));
 if (d != NULL)
 {
 d->name = strdup(name);
 d->age = age;
 d->owner = strdup(owner);
+}
+if (d->name == NULL || d->owner == NULL)
+{
+printf("faillure\n");
 }
 }
