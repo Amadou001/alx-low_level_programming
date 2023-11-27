52c735c26b03014cb72a8c68a415e6b035e3499b
Amadou001 <bamadou634@gmail.com>
2023-11-27 22:06:03 +0800

    C -Structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,21 @@
 #include "dog.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type stuct dog
- * @dog: first input
+ * @d: first input
  * @name: second input
  * @age: third input
  * @owner: fourth input
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
+d = (struct dog *)calloc(3,sizeof(struct dog));
+if (d != NULL)
+{
+d->name = strdup(name);
 d->age = age;
-d->owner = owner;
+d->owner = strdup(owner);
+}
 }
