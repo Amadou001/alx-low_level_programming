244fd2d22ca7de15cc5ef6c77efe3da21e9ee5cf
Amadou001 <bamadou634@gmail.com>
2023-11-30 01:13:45 +0800

    C -Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,18 +10,25 @@
 int main(int argc, char *argv[])
 {
 int num1, num2;
+char *op;
+op = argv[2];
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-if (!get_op_func(argv[2]))
+if (!get_op_func(op))
 {
 printf("Error\n");
 exit(99);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+{
+printf("Error\n");
+exit(100);
+}
 printf("%d\n", get_op_func(argv[2])(num1, num2));
 return (0);
 }
