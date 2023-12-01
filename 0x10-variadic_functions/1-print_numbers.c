5063b64e25c895e2920f5595e6742c67b1ca738e
Amadou001 <bamadou634@gmail.com>
2023-12-01 17:09:59 +0800

    C -Variadic Functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,11 +15,14 @@ if (separator == NULL)
 {
 return;
 }
-for (i = 0; i < n - 1; i++)
+for (i = 0; i < n; i++)
 {
-printf("%d%s", va_arg(args, int), separator);
-}
 printf("%d", va_arg(args, int));
+if (i != (n - 1))
+{
+printf("%s", separator);
+}
+}
 va_end(args);
 printf("\n");
 }
