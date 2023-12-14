8469f459fd4aa06bb4cd189f4abfee4050a79332
Amadou001 <bamadou634@gmail.com>
2023-12-14 14:52:58 +0800

    C -Singly linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * my_function - print a text before execution of main
+*/
+void my_function(void)
+{
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore my house upon my back!\n");
+}
