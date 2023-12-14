0c2d13331a10329f6f2fb2532505005a42c04be2
Amadou001 <bamadou634@gmail.com>
2023-12-14 14:29:44 +0800

    C -Singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: pointer to the list
+*/
+void free_list(list_t *head)
+{
+list_t *temp, *nexnode;
+if (head == NULL)
+{
+return ;
+}
+temp = head;
+while (temp != NULL)
+{
+nexnode = temp->next;
+free(temp->str);
+free(temp);
+temp = nexnode;
+}
+}
