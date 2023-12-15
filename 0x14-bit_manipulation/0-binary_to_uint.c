544b82c75e118976976bb77022e53b0070e5ae5d
Amadou001 <bamadou634@gmail.com>
2023-12-16 00:02:01 +0800

    C -bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,20 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-const char *temp;
-int i = 0;
 unsigned int result = 0;
-temp = b;
-if (temp == NULL)
+if (b == NULL)
 {
 return (0);
 }
-while (*(temp + i) != '\0')
+while (*b != '\0')
 {
-if (*(temp + i) != '0' && *(temp + i) != '1')
+if (*b != '0' && *b != '1')
 {
 return (0);
 }
 result <<= 1;
-result += *(temp + i) - '0';
-i++;
+result += *b - '0';
+b++;
 }
 return (result);
 }
