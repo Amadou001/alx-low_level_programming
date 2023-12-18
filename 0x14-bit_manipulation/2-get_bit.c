ec3de57e467c82a88d246776caa943b24335306c
Amadou001 <bamadou634@gmail.com>
2023-12-18 11:56:26 +0800

    C -bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,52 +7,10 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-int *convert;
-int result = 0;
-convert = binary(n);
-if (index > 32)
-{
-free(convert);
-return (-1);
-}
-if (convert == NULL)
+if (index >= (sizeof(unsigned long int) * 8))
 {
 return (-1);
 }
-result = convert[index];
-return (result);
-}
-/**
- * binary - converts the number in binary form
- * @n: the number to be converted
- * Return: the binary form of the number
-*/
-int *binary(unsigned long int n)
-{
-int *b1, *b2;
-int i = 0, j, temp = 0;
-b1 = (int *)malloc(sizeof(int) * 32);
-if (b1 == NULL)
-{
-return (NULL);
-}
-while (n > 0)
-{
-b1[i] = n % 2;
-n = n / 2;
-i++;
-}
-b2 = (int *)malloc(sizeof(int) * 32);
-if (b2 == NULL)
-{
-free(b1);
-return (NULL);
-}
-temp = i - 1;
-for (j = 0; j <= temp; j++)
-{
-b2[j] = b1[temp - j];
-}
-free(b1);
-return (b2);
+n >>= index;
+return (n & 1);
 }
