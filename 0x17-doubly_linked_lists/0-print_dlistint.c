dd2f8d944b1de310daaa3a96d9955fc11aa00964
Amadou001 <bamadou634@gmail.com>
2024-01-27 14:36:34 +0800

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - prints the element of a doubly linked list
+ * @h: pointer to the list
+ * Return: the number of elements
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 const dlistint_t *temp;
