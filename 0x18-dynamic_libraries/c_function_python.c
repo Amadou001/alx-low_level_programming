5e4ed42a5c3ab751e5a382de0bd6284d612e477b
Amadou001 <bamadou634@gmail.com>
2024-02-03 15:25:54 +0800

    C - Dynamic libraries along with python script

---
+++ 0x18-dynamic_libraries/c_function_python.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * add - adds two intergers
+ * @a: first input
+ * @b: second input
+ * Return: the sum
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * sub - substract two intergers
+ * @a: first input
+ * @b: second input
+ * Return: the difference
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * mul - multiplies two intergers
+ * @a: first input
+ * @b: second input
+ * Return: the result
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * div - divide two intergers
+ * @a: first input
+ * @b: second input
+ * Return: the result
+*/
+int div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * mod - calculates the remainder of div of two intergers
+ * @a: first input
+ * @b: second input
+ * Return: the result
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
