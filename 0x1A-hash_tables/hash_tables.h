38de0d1b6d7af7754f34860d4c9b62013233ecdf
Amadou001 <bamadou634@gmail.com>
2024-02-17 02:55:55 +0800

    C - Hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -45,5 +45,6 @@ const char *value,
 unsigned long int index);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
